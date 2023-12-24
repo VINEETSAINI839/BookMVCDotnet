2443d924f59c82f46f6e58baa8d54b061fc73f67
VINEETSAINI839 <vineetsaini839@gmail.com>
2023-12-24 14:44:11 +0530

    Add project files.

---
+++ BookMVCDotnet/Controllers/BooksController.cs
@@ -0,0 +1,89 @@
+﻿using BookMVCDotnet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMVCDotnet.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: BooksController
+        public ActionResult Index()
+        {
+            return View(Book.GetAllBooks());
+        }
+
+        // GET: BooksController/Details/5
+        public ActionResult Details(int id)
+        {
+            Book book=Book.GetBookDetails(id);
+            return View(book);
+        }
+
+        // GET: BooksController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BooksController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Book book,IFormCollection collection)
+        {
+            try
+            {
+                Book.AddBookDetails(book);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BooksController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(Book.GetBookDetails(id));
+        }
+
+        // POST: BooksController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Book book, IFormCollection collection)
+        {
+            try
+            {
+                Book.UpdateBookDetails(book);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BooksController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Book book = Book.GetBookDetails(id);
+            return View(book);
+        }
+
+        // POST: BooksController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                Book.DeleteBookDetails(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BookMVCDotnet/Models/Book.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace BookMVCDotnet.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string BookAuthor { get; set; }
+        public decimal  BookPrice { get; set; }
+
+        public static List<Book> GetAllBooks()
+        {
+            List<Book> list = new List<Book>();
+            SqlConnection cn = new SqlConnection();
+             cn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Books;Integrated Security=True;";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "getAllBooks";
+                SqlDataReader dr = cmd.ExecuteReader();
+                 while (dr.Read())
+                {
+                    list.Add(new Book() { BookId=dr.GetInt32(0),
+                        BookName=dr.GetString(1),
+                        BookAuthor=dr.GetString(2),
+                        BookPrice=dr.GetDecimal(3) });
+
+                }
+            }
+            catch {
+                throw;
+                   }
+            return list;
+        }
+        public static Book GetBookDetails(int bookId)
+        {
+            Book book = new Book();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Books;Integrated Security=True;";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "getBookDetails";
+                cmd.Parameters.AddWithValue("@BookId", bookId);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    book=new Book()
+                    {
+                        BookId = dr.GetInt32(0),
+                        BookName = dr.GetString(1),
+                        BookAuthor = dr.GetString(2),
+                        BookPrice = dr.GetDecimal(3)
+                    };
+
+                }
+            }
+            catch { throw; }
+            return book;
+        }
+
+        public static void UpdateBookDetails(Book book)
+        {
+            //Book book = new Book();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Books;Integrated Security=True;";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "UpdateBookDetails";
+               cmd.Parameters.AddWithValue("@BookId",book.BookId);
+                cmd.Parameters.AddWithValue("@BookName",book.BookName);
+                cmd.Parameters.AddWithValue("@BookAuthor", book.BookAuthor);
+                cmd.Parameters.AddWithValue("@BookPrice", book.BookPrice);
+                cmd.ExecuteNonQuery();
+               
+            }
+            catch { throw; }
+           
+        }
+
+
+        public static void AddBookDetails(Book book)
+        {
+            
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Books;Integrated Security=True;";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "AddBookDetails";
+                //cmd.Parameters.AddWithValue("@BookId",book.BookId);
+                cmd.Parameters.AddWithValue("@BookName", book.BookName);
+                cmd.Parameters.AddWithValue("@BookAuthor", book.BookAuthor);
+                cmd.Parameters.AddWithValue("@BookPrice", book.BookPrice);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch { throw; }
+
+        }
+
+        public static void DeleteBookDetails(int bookId)
+        {
+            Book book = new Book();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Books;Integrated Security=True;";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "deleteBookDetails";
+                cmd.Parameters.AddWithValue("@BookId", bookId);
+            
+                cmd.ExecuteNonQuery();
+
+            }
+            catch { }
+
+        }
+    }
+}
